8dbbae717ce4d14e3d71a8526e4767d6174fac7d
abdmmar <opowae909@gmail.com>
2024-11-01 21:40:56 +0700

    chore: lint

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-export default defineConfig({ plugins: [react()], resolve: {
-  alias: {
-    "@": path.resolve(__dirname, "./src"),
-  },
-} });
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
